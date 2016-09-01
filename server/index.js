b89b3b36af808ee51677838bd68e2c791a71dbdf
Jacob Avlund <jacob@avlund.dk>
2016-09-01 23:59:06 +0200

    Postgres example

--- server/index.js
+++ server/index.js
@@ -1,6 +1,20 @@
+const pg = require('pg');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 2500;
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if(err) {
+    throw err;
+  }
+  console.log('Connected to database');
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 app.use(express.static('.'));
 
