bafb2966180a0bfea426cf974d8b8d5e3d622600
Jacob Avlund <jacob@avlund.dk>
2016-10-27 10:54:56 +0200

    ID fix

--- server/paypal.js
+++ server/paypal.js
@@ -17,7 +17,7 @@ function receiveIPNData(req, res) {
       const id = body.custom;
       const now =  new Date().getTime();
       console.log(`Payment for member ${id} was completed.`);
-      members.child('id').update({lastpaid: now}, err => {
+      members.child(id).update({lastpaid: now}, err => {
         if(err) {
           console.log(`Could not update member ${id} with lastpaid set to ${now}, error is: ` + err);
           return;
