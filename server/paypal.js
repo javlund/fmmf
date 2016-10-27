ae0d14272d90d02a67bc2985b9013ba12aaed2e7
Jacob Avlund <jacob@avlund.dk>
2016-10-27 10:42:48 +0200

    Paypal receiver fix.

--- server/paypal.js
+++ server/paypal.js
@@ -3,30 +3,29 @@ const database = require('./database');
 
 const members = database.members;
 
-function receiveIPNData() {
-  return (req, res) => {
-    const body = req.body;
-    res.sendStatus(200);
-    ipn.verify(body, {allow_sandbox: true}, (err, message) => {
-      if(err) {
-        console.log(err);
-        return;
-      }
-      console.log(message);
-      if(body.payment_status === 'Completed') {
-        const id = body.custom;
-        const now =  new Date().getTime();
-        console.log(`Payment for member ${id} was completed.`);
-        members.child('id').update({lastpaid: now}, err => {
-          if(err) {
-            console.log(`Could not update member ${id} with lastpaid set to ${now}, error is: ` + err);
-            return;
-          }
-        });
+function receiveIPNData(req, res) {
+  const body = req.body;
+  res.sendStatus(200);
+  ipn.verify(body, {allow_sandbox: true}, (err, message) => {
+    if(err) {
+      console.log(err);
+      return;
+    }
+    console.log(message);
+    if(body.payment_status === 'Completed') {
+      const id = body.custom;
+      const now =  new Date().getTime();
+      console.log(`Payment for member ${id} was completed.`);
+      members.child('id').update({lastpaid: now}, err => {
+        if(err) {
+          console.log(`Could not update member ${id} with lastpaid set to ${now}, error is: ` + err);
+          return;
+        }
+      });
 
-      }
-    })
-  }
+    }
+  })
 }
+
 
 module.exports = receiveIPNData;
